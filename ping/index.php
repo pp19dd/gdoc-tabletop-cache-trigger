8fed31d8646addcecb498295862bd73aeaa0aec8
pp19dd <pp19dd@gmail.com>
2015-03-02 14:41:50 -0500

    Timestamps in error messages

--- ping/index.php
+++ ping/index.php
@@ -7,15 +7,22 @@
 // if everything checks out, runs fetch.rb and fetches a local data copy
 // ===========================================================================
 
+function error_message($msg) {
+    echo "<h1>ERROR</h1>\n";
+    echo "<p>" . date("r") . "</p>\n";
+    echo "<pre>" . $msg . "</pre>\n";
+    die;
+}
+
 // config must be set
 if( !file_exists( "../config.php") ) {
-    die( "ERROR: config.php is missing" );
+    error_message( "config.php is missing" );
 }
 require( "../config.php" );
 
 // key must be sent via $_GET
 if( !isset( $_GET['key']) ) {
-    die( "ERROR: key not specified" );
+    error_message( "key not specified" );
 }
 $key = trim(strip_tags($_GET['key']));
 
@@ -32,22 +39,22 @@ function verify_key($key) {
 }
 
 if( !verify_key($key) ) {
-    die( "ERROR: key {$key} is undefined" );
+    error_message( "key {$key} is undefined" );
 }
 
 // master key must be sent via $_POST
 if( !isset( $_POST['master_key']) ) {
-    die( "ERROR: master key not provided" );
+    error_message( "master key not provided" );
 }
 
 // master key must be set in config.php
 if( !isset( $config["main"]["master_key"] ) ) {
-    die( "ERROR: master key not set" );
+    error_message( "master key not set" );
 }
 
 // master keys must match
 if( $config["main"]["master_key"] !== $_POST["master_key"] ) {
-    die( "ERROR: provided master key doesn't match" );
+    error_message( "provided master key doesn't match" );
 }
 
 // ===========================================================================

c2936654c739f0de6ed3a82e4dd074cf87956b6b
pp19dd <pp19dd@gmail.com>
2015-03-03 09:21:05 -0500

    Node update 2

--- tabletop-node.js
+++ tabletop-node.js
@@ -1,15 +1,15 @@
 var Tabletop = require("./tabletop.js");
 
 if( process.argv.length != 3 ) {
-    console.info( "ERROR: need 3 parameters" );
+    console.info( "ERROR: need 3 parameters\n" );
+    console.info( "node tabletop-node.js [key] [callback]" );
     process.exit();
 }
 
 Tabletop.init({
     key: argv[2],
     callback: function(data, tabletop) {
-        //data_loaded(data);
-        console.info( "var " + argv[3] + " = " JSON.stringify(data) );
+        console.info( argv[3] + "(" + JSON.stringify(data) + ")" );
     },
     simpleSheet: true
 });
